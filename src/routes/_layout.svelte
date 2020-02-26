1187f07badfc1a4abcff8b8683a4123fd741c278
KeiferJu <jkf19980216@qq.com>
2020-02-26 19:36:40 +0800

    push rollup

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,21 +1,23 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-	export let segment;
+  import Nav from "../components/Nav.svelte";
+  export let segment;
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+<style lang="scss">
+  @import "../style/global.scss";   // 引入全局样式
+
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
