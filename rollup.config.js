88c31e66928461976e3693bda05bba1441241448
KeiferJu <jkf19980216@qq.com>
2020-03-03 16:55:38 +0800

    屏蔽css未使用警告

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,13 @@ const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const onwarn = (warning, onwarn) => {
+	if(warning.message === 'Unused CSS selector'){
+		return;
+	}else{
+		return (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning)
+	}
+};
 
 import sveltePreprocess from 'svelte-preprocess';
 const preprocess = sveltePreprocess({
